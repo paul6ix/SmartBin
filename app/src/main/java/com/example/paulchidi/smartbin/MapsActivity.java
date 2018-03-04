36fc2716fdc245afd655952ded8d55955b70f8e5
paul6ix <okporp@gmail.com>
2018-03-04 12:15:39 +0100

    Added a working Splash Screen

--- app/src/main/java/com/example/paulchidi/smartbin/MapsActivity.java
+++ app/src/main/java/com/example/paulchidi/smartbin/MapsActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,11 +22,21 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     //All declaration for the map
     private GoogleMap mMap;
-    public String url = "https://smart-bin-app.herokuapp.com/";
+    private static final String apiUrl = "https://smart-bin-app.herokuapp.com/";
+    private static final String consoleLog = "Data";
     // Bin coordinates -----------------------------------------------
     LatLng maryBin = new LatLng(6.671766, 3.157018);
     LatLng ubaBin = new LatLng(6.671851, 3.155241);
@@ -59,6 +70,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void binMarkers() {
+setupMap();
 
         MarkerOptions mapOptions = new MarkerOptions();
         String status = "medium";
@@ -91,13 +103,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .setItems(R.array.alert_otpions, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        if(which ==0){
-                           Toast.makeText(getApplicationContext(),R.string.alert_truck,Toast.LENGTH_LONG).show();
+                        if (which == 0) {
+                            Toast.makeText(getApplicationContext(), R.string.alert_truck, Toast.LENGTH_LONG).show();
 
-                        }
-                        else if(which==1){
-                            Toast.makeText(getApplicationContext(),"Plotting Route",Toast.LENGTH_LONG).show();
-                            Polyline line = mMap.addPolyline(new PolylineOptions().add(ubaBin,cafe1Bin).width(5).color(Color.BLUE) );
+                        } else if (which == 1) {
+                            Toast.makeText(getApplicationContext(), "Plotting Route", Toast.LENGTH_LONG).show();
+                            Polyline line = mMap.addPolyline(new PolylineOptions().add(ubaBin, cafe1Bin).width(5).color(Color.BLUE));
                         }
 
                     }
@@ -111,6 +122,72 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+    public void setupMap(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    retrieveData();
+                } catch (IOException e){
+                    Log.e(consoleLog,"Cannot retrieve map data");
+                    return;
+                }
+            }
+        }).start();
 
+    }
+
+    //retrieving data from api
+    protected void retrieveData() throws IOException {
+        // connecting to the web service
+        HttpURLConnection connection = null;
+        final StringBuilder json = new StringBuilder();
+        try {
+            URL url = new URL(apiUrl);
+            connection = (HttpURLConnection) url.openConnection();
+            InputStreamReader streamReader = new InputStreamReader(connection.getInputStream());
+            // Read the JSON data into the stringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = streamReader.read(buff)) != -1) {
+                json.append(buff, 0, read);
+
+            }
+
+        } catch (IOException e) {
+            Log.e(consoleLog, "Error connecting to service ", e);
+            throw new IOException("Error connecting to service", e);
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    makeMarkers(json.toString());
+
+                } catch (JSONException e) {
+                    Log.e(consoleLog, "Error processing JSON", e);
+                }
+            }
+        });
+    }
+
+    void makeMarkers(String json) throws JSONException {
+        JSONArray jsonArray = new JSONArray(json);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            mMap.addMarker(new MarkerOptions()
+                    .title(jsonObject.getString("name"))
+                    .snippet(jsonObject.getString("status"))
+                    .position(new LatLng(
+                            jsonObject.getJSONArray("coordinates").getDouble(0),
+                            jsonObject.getJSONArray("coordinates").getDouble(1)
+                    )));
+        }
+
+    }
 
 }
